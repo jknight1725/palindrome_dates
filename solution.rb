91abbfb6dc4c0f26cf9ec321288c1c2ac7bebf1e
James Knight <james.p.knight2@gmail.com>
2020-02-10 10:20:35 -0700

    Create solution.rb

---
+++ solution.rb
@@ -0,0 +1,47 @@
+require 'date'
+
+def palindrome?(str)
+  str == str.reverse
+end
+
+def dates_to_check
+  palindrome_years = []
+  1000.upto(10_000) do |year|
+    year = year.to_s
+    year.prepend year.reverse
+    palindrome_years << year if palindrome? year
+  end
+  palindrome_years
+end
+
+def valid_month(arr)
+  arr.filter! { |x| (x[0]+x[1]).to_i > 1 && (x[0]+x[1]).to_i < 13 }
+end
+
+def valid_day(arr)
+  arr.filter! { |x| (x[2]+x[3]).to_i > 1 && (x[2]+x[3]).to_i < 32 }
+end
+
+def filter_date(arr)
+  valid_month arr
+  valid_day arr
+end
+
+def format_date(arr)
+  arr.map { |x| x.insert(2, '/' ); x.insert(5, '/' ) }
+end
+
+def valid_date?(date)
+  date_format = '%m/%d/%Y'
+  DateTime.strptime(date, date_format)
+  true
+rescue ArgumentError
+  false
+end
+
+def solution
+  arr = format_date(filter_date(dates_to_check))
+  arr.filter(&method(:valid_date?))
+end
+
+puts solution
